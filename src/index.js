a5abc314c681825ee665e457253cc9cce668f934
Jorge Fuente-Alba <AsPerSpecUnity@gmail.com>
2020-01-21 14:31:01 -0300

    Fixed error handling for hook

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import { skip } from 'rxjs/operators';
 
 export const useSubject = (subject,onError,onComplete) => {
-  const [value, setValue] = useState(subject.getValue());
+  const [value, setValue] = useState(subject.hasError?null:subject.getValue());
   useEffect(() => {
     const subFn = { next: data => setValue(data) };
     if (onError) { subFn.error = err => onError(err); }
